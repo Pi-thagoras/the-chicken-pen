f9b3dfb2f0d1e89e3680eb79c865431408e60bee
Pi-thagoras <pi.thagoras169@gmail.com>
2023-11-09 12:25:20 +0800

    Revert

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const links = document.querySelectorAll("ul li a");
 const navlinks = document.querySelectorAll("ul li");
 
 document.addEventListener("mousemove", (e) => {
-    let x = e.windowX + 4;
-    let y = e.windowY + 4;
+    let x = e.pageX + 4;
+    let y = e.pageY + 4;
     console.log(x,y);
     cursor.style.left = x + "px";
     cursor.style.top = y + "px";
